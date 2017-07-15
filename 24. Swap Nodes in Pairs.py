dbeb5af3816181655c16fb65435d985d42dfb4be
chenshenyuan <chenshenyuan1990@gmail.com>
2017-07-15 15:57:38 -0400

    24. Swap Nodes in Pairs

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -1,3 +1,25 @@
+# still have no idea about what happend here, just post answer I found
 
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
 
 
